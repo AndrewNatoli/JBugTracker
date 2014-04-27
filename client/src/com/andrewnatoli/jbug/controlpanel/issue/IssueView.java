081ea42551361489eb8d18a5b74fc57e91103816
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2014-04-27 18:16:48 -0400

    Starting to re-work the IssueView to make issues editable. (Not done yet)

--- client/src/com/andrewnatoli/jbug/controlpanel/issue/IssueView.java
+++ client/src/com/andrewnatoli/jbug/controlpanel/issue/IssueView.java
@@ -1,8 +1,12 @@
 package com.andrewnatoli.jbug.controlpanel.issue;
 
+import com.andrewnatoli.jbug.authentication.CurrentUser;
+import com.andrewnatoli.jbug.controlpanel.ControlPanelView;
 import com.andrewnatoli.jbug.controlpanel.user.UserModel;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class IssueView extends JPanel {
@@ -27,14 +31,28 @@ public class IssueView extends JPanel {
     protected JPanel issueTopRow;
     protected JPanel issueTitlePanel;
     protected JPanel issueStatusPanel;
-    protected JLabel issueTitle;
+    protected JTextArea issueTitle;
     protected JLabel issueOpenLabel;
 
     /**
      * GUI elements for for the issueInfo JPanel
      */
-    protected JLabel issueDescription;
+    protected JTextArea issueDescription;
     protected JLabel issuePostedBy;
+
+    /**
+     * Combo-boxes and their strings
+     */
+    protected JComboBox issueStatus;
+    protected JComboBox priorityLevel;
+    String[] issueStatusStrings = { "Closed","Open" };
+    String[] issuePriorityStrings = { "0 - Feature Request","1 - Low Priority","2","3","4","5","6","7","8","9 - Urgent"};
+
+    /**
+     * Button to revise the ticket
+     */
+    protected JButton btn_reviseTicket;
+    protected JButton btn_saveChanges;
 
     /**
      * Constructor of Justice - Requires an issue_id passed to it so we know what issue to look up.
@@ -57,18 +75,27 @@ public class IssueView extends JPanel {
         //Title Panel
         issueTitlePanel = new JPanel();
         issueTitlePanel.setBorder(BorderFactory.createTitledBorder("Issue #" + issue.getIssue_id()));
-        issueTitlePanel.setPreferredSize(new Dimension(449,50));
-        issueTitle = new JLabel(formatIssueText(issue.getTitle()));
+        issueTitlePanel.setPreferredSize(new Dimension(449, 50));
+        issueTitle = new JTextArea();
+        issueTitle.setEditable(false);
+        issueTitle.setOpaque(false);
+        issueTitle.setText(issue.getTitle());
         issueTitlePanel.add(issueTitle);
 
         //Priority & Open/Closed status
         issueStatusPanel = new JPanel();
         issueStatusPanel.setBorder(BorderFactory.createTitledBorder("Priority: " + issue.getPriority()));
-        issueStatusPanel.setPreferredSize(new Dimension(145,50));
+        issueStatusPanel.setPreferredSize(new Dimension(145, 50));
         issueOpenLabel = new JLabel(getOpenStatus());
         issueOpenLabel.setFont(new Font("Sans-Serif", Font.PLAIN, 24));
 
-        issueStatusPanel.add(issueOpenLabel);
+        issueStatus = new JComboBox(issueStatusStrings);
+        issueStatus.setOpaque(false);
+        issueStatus.setSelectedIndex(issue.getOpen());
+        issueStatus.setEditable(false);
+
+
+        issueStatusPanel.add(issueOpenLabel); //Switch it out for issueStatus combobox when we edit the issue
         //Add them to the top row and we're done here.
         issueTopRow.add(issueTitlePanel,BorderLayout.CENTER);
         issueTopRow.add(issueStatusPanel,BorderLayout.EAST);
@@ -79,7 +106,14 @@ public class IssueView extends JPanel {
         issueDescriptionPanel.setBorder(BorderFactory.createTitledBorder("Description"));
         issueDescriptionPanel.setLayout(new BorderLayout());
 
-        issueDescription = new JLabel(formatIssueText(issue.getDescription()) + "\n");
+        issueDescription = new JTextArea(30,10);
+        issueDescription.setEditable(false);
+        issueDescription.setOpaque(false);
+        issueDescription.setLineWrap(true);
+        issueDescription.setWrapStyleWord(true);
+        issueDescription.setLineWrap(true);
+        issueDescription.setText(issue.getDescription());
+
         issuePostedBy = new JLabel("Reported by " + author.getName());
 
         issueDescriptionPanel.add(issueDescription,BorderLayout.CENTER);
@@ -88,6 +122,22 @@ public class IssueView extends JPanel {
         //Adds the issueInfo panel to our main, IssueView JPanel
         add(issueTopRow);
         add(issueDescriptionPanel);
+
+        //The "Revise Ticket" button... if the issue belongs to them
+        if(issue.getUser_id() == CurrentUser.getUser_id()) {
+            btn_reviseTicket = new JButton("Revise Ticket");
+            btn_saveChanges  = new JButton("Save Changes");
+            btn_reviseTicket.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    System.out.println("Clicked button to revise ticket");
+                    remove(btn_reviseTicket);
+                    add(btn_saveChanges);
+                    ControlPanelView.controlPanelFrame.pack();
+                }
+            });
+            add(btn_reviseTicket);
+        }
 
         System.out.println("Boom, IssueView is visible. Enjoy it.");
     }
